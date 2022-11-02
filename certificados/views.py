47c028ebbb604183cd90c9db7e043ad3313b8f3c
Julio Facal <starlyn.dev@gmail.com>
2022-11-01 22:07:52 -0300

    autenticacao basica

--- certificados/views.py
+++ certificados/views.py
@@ -1,9 +1,13 @@
 from certificados.models import Certificado
 from certificados.serializer import CertificadoSerializer
 from rest_framework import viewsets
-
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class CertificadosViewSet(viewsets.ModelViewSet):
+    
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     
     queryset = Certificado.objects.all()
     
